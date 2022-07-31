a2ebfaaf83ff333878a567785a1e2fd4fb6ac6da
Richard Škoda <richard.skoda72@gmail.com>
2022-07-31 20:44:55 +0200

    Moving text in sidebar is now function. Do some on sidebar hiding while refresh page (incomplete, but I wrote some idea how to do to ursulus.js)

--- ursulus.js
+++ ursulus.js
@@ -5,29 +5,80 @@ let logoBtn = document.querySelector('.main-logo');
 let galeryBtn = document.querySelector('#galery-btn');
 let contactBtn = document.querySelector('#contact-btn');
 
-// add conditions to move sidebar only by first click
-galeryBtn.onclick = function(event) {
-  event.preventDefault();     // how to refresh page and move sidebar together???
-  console.log(sidebar);
-  sidebar.classList.toggle('active');
-}
-
+// try to move sidebar according page title
+const galeryTitle = document.querySelector('#title');
 
 function removeSidebar() {
   event.preventDefault();
-  console.log(sidebar);
   sidebar.classList.remove('active');
 };
 
-mainBtn.onclick = function() {
+/* co takhle udelat 2x sidebar.css. 1x pouzit aktualni pro galery a pro ostatni pouzit to same, ale otocene chovani pro active a non active
+takto by bylo pro ostatni
+
+.sidebar ul li {
+  margin-left: 0;
+  list-style-type: none;
+  cursor: pointer;
+  padding: 25px 10px;
+  transition: all 0.7s ease;
+}
+
+.sidebar.active ul li {       tady mozna napsat deactive, at se to pak v js neplete
+  margin-left: -15px;
+}
+*/
+
+function enableSidebar() {
+  console.log(galeryTitle.text)
+  console.log(sidebar)
+  if (galeryTitle.text === 'Ursulus- galery' && sidebar.classList.length === 1) {
+    sidebar.classList.toggle('active');
+    console.log(sidebar)
+  };
+  if (galeryTitle.text != 'Ursulus- galery') {
+    sidebar.classList.toggle('active');
+  }
+};
+
+enableSidebar();
+
+
+// funguje, ale nerefresuje stranku. kdzy ano, nevylejza sidebar
+
+/*
+function enableSidebar() {
+  event.preventDefault();
+  if (sidebar.classList.length === 1) {
+    sidebar.classList.toggle('active');
+  };
+};
+
+function removeSidebar() {
+  event.preventDefault();
+  sidebar.classList.remove('active');
+};
+
+galeryBtn.onclick = function() {
+  setTimeout(function(){ location.reload() }, 3000);
+  enableSidebar()
+};
+
+mainBtn.onclick = function() {    // how to refresh page and move sidebar together???
+  setTimeout(function(){ location.reload() }, 3000);
   removeSidebar()
 };
 productionBtn.onclick = function() {
+  setTimeout(function(){ location.reload() }, 3000);
   removeSidebar()
 };
 logoBtn.onclick = function() {
+  setTimeout(function(){ location.reload() }, 3000);
   removeSidebar()
 };
 contactBtn.onclick = function() {
+  setTimeout(function(){ location.reload() }, 3000);
   removeSidebar()
 };
+
+*/
