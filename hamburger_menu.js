7cc89cb5af65b13a3e38d214d8ac9578db868767
Richard Škoda <richard.skoda72@gmail.com>
2022-09-01 16:48:12 +0200

    Remove moving of hamburger menu in mobile header.

--- hamburger_menu.js
+++ hamburger_menu.js
@@ -8,11 +8,11 @@ const navigationBtn = document.querySelectorAll('.sidebar-link');
 hamburgerLink.addEventListener('click', function () {
   hamburgerMenu.classList.toggle('active');
   // moveble hambuirger menu or not?????
-  if (hamburgerMenu.classList[1] === 'active') {
-    hamburgerIconContainer.style.marginLeft = '27%';
-  } else {
-    hamburgerIconContainer.style.marginLeft = '2%';
-  }
+  // if (hamburgerMenu.classList[1] === 'active') {
+  //   hamburgerIconContainer.style.marginLeft = '27%';
+  // } else {
+  //   hamburgerIconContainer.style.marginLeft = '2%';
+  // }
 })
 
 console.log(navigationBtn)
